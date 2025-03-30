979f6c643b1b7ddc2b50b8932072ecc4e24fe7bf
NoahSchiro <noahschiro@rocketmail.com>
2025-03-30 12:53:41 -0400

    Remove error on startup:

--- init.lua
+++ init.lua
@@ -111,12 +111,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    main = "ibl",
-    opts = {}
-  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
